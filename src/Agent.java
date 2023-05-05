9a9aa0314a52eaf7bca31d3da17ec271e9263873
davide <davidedeblasio2003@gmail.com>
2023-05-05 15:16:56 +0200

    Added agent (generate public and private key)

---
+++ src/Agent.java
@@ -0,0 +1,27 @@
+import java.security.*;
+
+public class Agent {
+    private PublicKey publicKey;
+    private PrivateKey privateKey;
+
+    public Agent() {
+        try {
+            // Generate key pair
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(2048);
+            KeyPair keyPair = keyGen.generateKeyPair();
+            this.publicKey = keyPair.getPublic();
+            this.privateKey = keyPair.getPrivate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public PublicKey getPublicKey() {
+        return this.publicKey;
+    }
+
+    public PrivateKey getPrivateKey() {
+        return this.privateKey;
+    }
+}
